03f0606ffdcd536d95b89121d5e5e4cf9a5294cb
Markis Taylor <m@rkis.net>
2023-03-03 21:23:08 -0500

    refactor: introduce config/context dependencies

---
+++ hubs_bot/__init__.py
@@ -0,0 +1,6 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("hubs-bot")
+
+logger.info("starting")
---
+++ hubs_bot/config.py
@@ -0,0 +1,42 @@
+import os
+from collections.abc import Callable, Mapping
+from dataclasses import dataclass, field
+
+BASE_URL = "https://www.beaconjournal.com"
+NEWS_TAGS = {"LOCAL", "HUDSON HUB TIMES"}
+
+
+def create_set_factory(
+    env_name: str, default: set[str], env: Mapping[str, str | set[str]]
+) -> Callable[[], set[str]]:
+    def _get_set() -> set[str]:
+        value = env.get(env_name, default)
+        if isinstance(value, str):
+            return set(value.split(","))
+        elif isinstance(value, set):
+            return value
+        else:
+            raise ValueError(f"${env_name} is not set correctly")
+
+    return _get_set
+
+
+@dataclass
+class Config:
+    base_url: str = os.environ.get("BASE_URL", BASE_URL)
+    hubtimes_url: str = os.environ.get("HUBTIMES_URL", f"{BASE_URL}/communities/hudsonhubtimes/")
+    subreddit: str = os.environ.get("SUBREDDIT", "hudsonohtest")
+    subreddit_flair: str = os.environ.get("SUBREDDIT_FLAIR", "93312688-b815-11ea-917e-0e65e9cea44f")
+    news_tags: set[str] = field(
+        default_factory=create_set_factory("NEWS_TAGS", NEWS_TAGS, os.environ)
+    )
+    username: str = os.environ.get("USERNAME", "hubs-bot")
+    password: str = os.environ.get("PASSWORD", "")
+    client_id: str = os.environ.get("CLIENT_ID", "")
+    client_secret: str = os.environ.get("CLIENT_SECRET", "")
+
+    def __post_init__(self) -> None:
+        assert self.client_id, "$CLIENT_ID needs to be set"
+        assert self.client_secret, "$CLIENT_SECRET needs to be set"
+        assert self.password, "$PASSWORD needs to be set"
+        assert self.username, "$USERNAME needs to be set"
